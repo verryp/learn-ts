6db9787b1183c084136889798be0ad41ff826e25
verryp <verryp.dev@gmail.com>
2020-03-21 16:07:06 +0700

    basic type

---
+++ build/basicType.js
@@ -0,0 +1,17 @@
+"use strict";
+// ! String type
+var myName;
+myName = 'Verry P';
+// ! Number type
+var year;
+year = 23;
+// ! Boolean type
+var isSingle;
+isSingle = true;
+// ! Any type
+var randomVal;
+randomVal = 23;
+randomVal = "Types";
+randomVal = true;
+randomVal = [];
+randomVal = {};
---
+++ src/basicType.ts
@@ -0,0 +1,19 @@
+// ! String type
+let myName: string
+myName = 'Verry P'
+
+// ! Number type
+let year: number
+year = 23
+
+// ! Boolean type
+let isSingle: boolean
+isSingle = true
+
+// ! Any type
+let randomVal: any
+randomVal = 23
+randomVal = "Types"
+randomVal = true
+randomVal = []
+randomVal = {}
