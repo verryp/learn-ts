f9bb59be366e08a7746797d9efac63dafc00dd1d
verryp <verryp.dev@gmail.com>
2020-03-21 20:44:57 +0700

    object data type

---
+++ build/object.js
@@ -0,0 +1,8 @@
+"use strict";
+let user;
+user = {
+    name: "Verry P",
+    age: 22,
+    profession: "Web Developer"
+};
+console.log(user.name);
---
+++ src/object.ts
@@ -0,0 +1,15 @@
+type User = {
+    name: string
+    age: number
+    profession: string
+}
+
+let user: User
+
+user = {
+    name: "Verry P",
+    age: 22,
+    profession: "Web Developer"
+}
+
+console.log(user.name);
