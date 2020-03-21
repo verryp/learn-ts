d239c5f4d406abf40c50cd45ab06f8f84e030cf0
verryp <verryp.dev@gmail.com>
2020-03-21 18:07:14 +0700

    data type for argumnets

---
+++ build/functionArguments.js
@@ -0,0 +1,6 @@
+"use strict";
+function multiply(temp1, temp2, temp3) {
+    return temp1 * temp2 * temp3;
+}
+const results = multiply(2, 2, 5);
+console.log(results);
---
+++ src/functionArguments.ts
@@ -0,0 +1,7 @@
+function multiply(temp1: number, temp2: number, temp3: number): number {
+    return temp1 * temp2 * temp3
+}
+
+const results = multiply(2, 2, 5)
+
+console.log(results);
