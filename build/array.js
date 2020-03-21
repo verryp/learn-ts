16258070bdfe4644b23f0dbc58b07523ba694ff3
verryp <verryp.dev@gmail.com>
2020-03-21 16:16:22 +0700

    array data type

---
+++ build/array.js
@@ -0,0 +1,13 @@
+"use strict";
+// ! Array data type
+var arrayNumber;
+arrayNumber = [1, 2, 2];
+var arrayString;
+arrayString = ["A", "B", "C"];
+var arrayRandomVal;
+arrayRandomVal = [1, "test", true, {
+        "test": "banana"
+    }];
+// Tuples
+var arrayNumberString;
+arrayNumberString = ["String", 123];
---
+++ src/array.ts
@@ -0,0 +1,15 @@
+// ! Array data type
+let arrayNumber: number[]
+arrayNumber = [1,2,2]
+
+let arrayString: string[]
+arrayString = ["A", "B", "C"]
+
+let arrayRandomVal: any[]
+arrayRandomVal = [1, "test", true, {
+    "test": "banana"
+}]
+
+// Tuples
+let arrayNumberString: [string, number]
+arrayNumberString = ["String", 123]
